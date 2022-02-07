596433b670b07065a49b7b2c991969156f36c2f8
INDRA\fgrubio <fgrubio@minsait.com>
2022-02-07 12:40:23 +0100

    Sección 12 - Google

--- 04-backend-server/controllers/auth.js
+++ 04-backend-server/controllers/auth.js
@@ -3,17 +3,18 @@ const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+// const { googleVerify } = require('../helpers/google-verify');
 
-const login = async(req,res) => {
+const login = async (req, res) => {
 
     const { email, password } = req.body;
 
-    try{
+    try {
 
         //verificar email
         const usuarioDB = await Usuario.findOne({ email });
 
-        if(!usuarioDB){
+        if (!usuarioDB) {
             return res.status(404).json({
                 ok: false,
                 msg: "email no encontrado"
@@ -22,8 +23,8 @@ const login = async(req,res) => {
 
         // verificar contraseña
         const validPassword = bcrypt.compareSync(password, usuarioDB.password);
-        
-        if(!validPassword){
+
+        if (!validPassword) {
             return res.status(400).json({
                 ok: false,
                 msg: "contraseña incorrecta"
@@ -32,14 +33,14 @@ const login = async(req,res) => {
 
         // generar token
         const token = await generarJWT(usuarioDB.id);
-        
+
 
 
         res.json({
             ok: true,
             token
         })
-    }catch(error){
+    } catch (error) {
         console.log(error);
         res.status(500).json({
             ok: false,
@@ -48,7 +49,57 @@ const login = async(req,res) => {
     }
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try {
+    //    const {name, email, pincture } = await googleVerify(googleToken);
+
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if( !usuarioDB ){
+            // si no existe el usuario se crea
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: pincture,
+                google: true
+            });
+        }else{
+            // si existe el usuario se actualiza
+            usuario = usuarioDB;
+            usuario.google = true;
+            usuario.password = '@@@';
+        }
+
+        // guardar usuario
+        await usuario.save();
+
+        
+        // generar token
+        const token = await generarJWT(usuarioDB.id);
+
+
+        res.json({
+            ok: true,
+            token
+
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            ok: false,
+            msg: 'token no correcto'
+        });
+    }
+
+}
+
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
